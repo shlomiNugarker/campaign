d1d514fbd0c331ab110a840f3e471fd602996ece
Shlomi Nugarker <shlomin1231@gmail.com>
2022-12-01 19:18:13 +0200

    c

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 'use strict'
 
 function sendEmail(ev) {
-  // ev.preventDefault()
+  ev.preventDefault()
+  const nameEl = document.querySelector('.fullname')
+  const phoneEl = document.querySelector('.phone')
+  const companyEl = document.querySelector('.company')
 
   Email.send({
     SecureToken: '09e66569-b94f-4417-af01-f841cdce0054',
     To: 'zoharnogerker@gmail.com',
     From: 'zoharnogerker@gmail.com',
-    Subject: 'שלום זוהר, הודעה חדשה מהאתר נחיתה',
-    Body: 'מה נישמע זה אני מהאתר נרשמתי',
-  }).then((message) => alert(message))
+    Subject: 'שלום זוהר,    הודעה חדשה מהאתר נחיתה',
+    Body: `שם: ${nameEl.value} ,    טלפון: ${phoneEl.value},    חברה: ${companyEl.value}`,
+  }).then((res) => {
+    console.log(res)
+    nameEl.value = ''
+    phoneEl.value = ''
+    companyEl.value = ''
+  })
 }
